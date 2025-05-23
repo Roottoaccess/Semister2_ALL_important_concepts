040c3f8ba69caf851855850954400a0aa225b28c
Roottoaccess <network123crab@gmail.com>
2025-05-23 12:06:28 +0530

    java script for myself..

---
+++ practise waf/javascript/first.js
@@ -0,0 +1,24 @@
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+
+console.log(product);
+
+// Create a const object called "profile" to share information shown in the picture.
+
+const profile = {
+    name: "shradhakhapra",
+    isFollow: true,
+    post: 195,
+    followers: '569k',
+    following: 4,
+    tag: '@shradhakapra',
+    bio: 'Apna Collage| Ex-Microsoft DRDO To educate someone is the highest privilage ❤️'
+};
+
+// document.write(profile.name+"<br>"+profile.post)
+console.log(profile)
+console.log(typeof profile['tag'])
---
+++ practise waf/javascript/second.js
@@ -0,0 +1,12 @@
+/* This is a multi-line comment */
+// This is a single line comment
+
+// Operators to perform operation in the javascript helps up to perform operation 
+
+// OPerator and operands -> Arithmetic operator
+
+let a = 2;
+let b = 5;
+console.log("a = ", a, "b = ", b);
+console.log("a + b = ",a + b);
+console.log("a - b = ",a - b);
