b1523af717a1e71fadfccf0c437ad36cf19d3519
Roottoaccess <network123crab@gmail.com>
2025-05-30 17:10:41 +0530

    javascript for exam and learning the basic contents

---
+++ WAF/own_javaScript/fourth.js
@@ -0,0 +1,7 @@
+// This is the script for applying the string concepts
+
+let str1 = "Daneal Services";
+console.log("The payload name is: ",str1);
+
+let specialString = `this is a template literal`;
+console.log('This is the special String',typeof specialString)
