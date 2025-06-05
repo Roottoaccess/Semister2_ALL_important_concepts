aa56db24afedd9e3664fa058479d47172ba5fd24
Roottoaccess <network123crab@gmail.com>
2025-06-05 14:48:58 +0530

    all the javascript codes before exam....

--- WAF/javascript/Document_Object_model/events.js
+++ WAF/javascript/Document_Object_model/events.js
@@ -1,11 +1,28 @@
 // Scripting using JavaScript
 let btn1 = document.querySelector("#btn1");
-btn1.onclick = () => {
-    console.log("clicked !!");
-    alert("click the normal button !");
-}
+// btn1.onclick = () => {
+//     console.log("clicked !!");
+//     alert("click the normal button !");
+// }
 
 let box = document.querySelector("div");
-box.onmouseover = () => {
-    console.log("you are inside the div box");
-}
+// box.onmouseover = () => {
+//     console.log("you are inside the div box");
+// }
+
+// let bbnn = document.createElement('button');
+// bbnn.innerText = "server connection";
+// document.querySelector("body").append(bbnn);
+// bbnn.style.backgroundColor = 'yellow';
+// bbnn.style.color = "red";
+
+// let bn = document.querySelector("bbnn");
+// bn.onclick = () => {
+//     console.log("server button clicked");
+//     alert("server started");
+// }
+
+// doing the same work with the help of Event Listener
+btn1.addEventListener("click", () => {
+    console.log('button was clicked');
+})
---
+++ WAF/own_javaScript/pic.js
@@ -0,0 +1,9 @@
+function skeli(){
+    document.getElementById("fun").src = "dancing.gif";
+    document.getElementById("fun").alt = "sketon_dancing";
+}
+
+function medi(){
+    document.getElementById("fun").src = "still.gif";
+    document.getElementById("fun").alt = 'meditaion chanting';
+}
